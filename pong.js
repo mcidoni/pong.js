3134db6d2d9c9eb127eb542a071c0619c28bf1af
mcidoni <matthewcidoni@gmail.com>
2021-02-26 17:53:59 -0500

    fix ball hitting walls exactly]

--- pong.js
+++ pong.js
@@ -21,15 +21,15 @@ class Rect
   }
   get right() 
   {
-    return this.pos.x - this.size.x / 2;
+    return this.pos.x + this.size.x / 2;
   }
-  get left() 
+  get top() 
   {
     return this.pos.y - this.size.y / 2;
   }
-  get right() 
+  get bottom() 
   {
-    return this.pos.y - this.size.y / 2;
+    return this.pos.y + this.size.y / 2;
   }
 }
 
@@ -65,18 +65,18 @@ function update(dt) {
   ball.pos.x += ball.vel.x * dt;
   ball.pos.y += ball.vel.y * dt;
   
+  if (ball.left < 0 || ball.right > canvas.width) {
+    ball.vel.x = -ball.vel.x;
+  }
+  if (ball.top < 0 || ball.bottom > canvas.height) {
+    ball.vel.y = -ball.vel.y;
+  }
+
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
   
   context.fillStyle = '#fff';
   context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y);
-
-  if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
-    ball.vel.x = -ball.vel.x;
-  }
-  if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
-    ball.vel.y = -ball.vel.y;
-  }
 }
 
 callBack();
