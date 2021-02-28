1d984127042cdb871c7c1edf07d48e5f4af16d1f
mcidoni <matthewcidoni@gmail.com>
2021-02-28 17:22:42 -0500

    add score functionality, reset method

--- pong.js
+++ pong.js
@@ -59,10 +59,7 @@ class Pong
     this._context = canvas.getContext('2d');
 
     this.ball = new Ball;
-    this.ball.pos.x = 100;
-    this.ball.pos.y = 50;
-    this.ball.vel.x = 100;
-    this.ball.vel.y = 100;
+    
 
     this.players = [
       new Player,
@@ -85,7 +82,17 @@ class Pong
       requestAnimationFrame(callBack);
     }
     callBack();
+
+    this.reset();
   }
+
+  collide(player, ball)
+  {
+    if (player.left < ball.right && player.right > ball.left && player.top < ball.bottom && player.bottom > ball.top) {
+      ball.vel.x = -ball.vel.x;
+    }
+  }
+
   draw() 
   {
     this._context.fillStyle = '#000';
@@ -93,10 +100,19 @@ class Pong
     this.drawRect(this.ball);
     this.players.forEach(player => this.drawRect(player));
   }
+
   drawRect(rect)
   {
     this._context.fillStyle = '#fff';
     this._context.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
+  }
+
+  reset() 
+  {
+    this.ball.pos.x = 100;
+    this.ball.pos.y = 50;
+    this.ball.vel.x = 100;
+    this.ball.vel.y = 100;
   }
 
   update(dt) {
@@ -104,13 +120,19 @@ class Pong
     this.ball.pos.y += this.ball.vel.y * dt;
     
     if (this.ball.left < 0 || this.ball.right > this._canvas.width) {
-      this.ball.vel.x = -this.ball.vel.x;
+      const playerId = this.ball.vel.x < 0 | 0;
+      this.players[playerId].score++;
+      this.reset();
     }
+
     if (this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
       this.ball.vel.y = -this.ball.vel.y;
     }
 
-    this.players[1].pos.y = this.ball.pos.y
+    this.players[1].pos.y = this.ball.pos.y;
+
+    this.players.forEach(player => this.collide(player, this.ball));
+
     this.draw();
   }
 }
